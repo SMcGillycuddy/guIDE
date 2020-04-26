a1d409dc209adec05100bccede7d1ccc295bc9b6
SMcGillycuddy <seadnamcgillycuddy@gmail.com>
2020-04-26 19:33:58 +0100

    Added Find Text functionality

--- guIDE.py
+++ guIDE.py
@@ -25,11 +25,7 @@ root.title(PROGRAM_NAME)
 #adding an icon to the window
 root.iconphoto(False, PhotoImage(file='icons/guIDE_logo_30.png'))
 
-def select_all(event=None):
-	content_text.tag_add('sel', '1.0', 'end')
-	return "break"
-
-#functions that link the text operations to their relative menu buttons
+#defining the Cut, Copy, Paste, Undo, Redo functions
 def cut():
 	content_text.event_generate("<<Cut>>")
 
@@ -45,6 +41,52 @@ def undo():
 def redo(event=None):
 	content_text.event_generate("<<Redo>>")
 	return 'break'
+
+#defining the select all function
+def select_all(event=None):
+	content_text.tag_add('sel', '1.0', 'end')
+	return "break"
+
+#defining the find text function
+def find_text(event=None):
+	search_toplevel = Toplevel(root)
+	search_toplevel.title("Find Text")
+	search_toplevel.transient(root)
+	search_toplevel.resizable(False, False)
+	Label(search_toplevel, text="Find All:").grid(row=0, column=0, sticky='e')
+	search_entry_widget = Entry(search_toplevel, width=25)
+	search_entry_widget.grid(row=0, column=1, padx=2, pady=2, sticky='we')
+	search_entry_widget.focus_set()
+	ignore_case_value = IntVar()
+	Checkbutton(search_toplevel, text='Ignore Case', variable=ignore_case_value).grid(row=1, column=1, sticky='e', padx=2, pady=2)
+	Button(search_toplevel, text="Find All", underline=0, command=lambda: search_output(search_entry_widget.get(), ignore_case_value.get(), content_text, search_toplevel, search_entry_widget)).grid(row=0, column=2, sticky='e' + 'w', padx=2, pady=2)
+
+#defining the close search window function
+def close_search_window():
+	content_text.tag_remove('match', '1,0', END)
+	search_toplevel.destroy()
+	search_toplevel.protocol('WM_DELETE_WINDOW', close_search_window)
+	return 'break'
+
+#defining the search output function
+def search_output(needle, if_ignore_case, content_text,
+                  search_toplevel, search_box):
+    content_text.tag_remove('match', '1.0', END)
+    matches_found = 0
+    if needle:
+        start_pos = '1.0'
+        while True:
+            start_pos = content_text.search(needle, start_pos, nocase=if_ignore_case, stopindex=END)
+            if not start_pos:
+                break
+            end_pos = '{}+{}c'.format(start_pos, len(needle))
+            content_text.tag_add('match', start_pos, end_pos)
+            matches_found += 1
+            start_pos = end_pos
+        content_text.tag_config(
+            'match', foreground='red', background='yellow')
+    search_box.focus_set()
+    search_toplevel.title('{} matches found'.format(matches_found))
 
 
 #specifing the icons for the menu
@@ -79,7 +121,7 @@ edit_menu.add_command(label='Cut', accelerator='Ctrl+X', compound='left', image=
 edit_menu.add_command(label='Copy', accelerator='Ctrl+C', compound='left', image=copy_icon, command=copy)
 edit_menu.add_command(label='Paste', accelerator='Ctrl+V', compound='left', image=paste_icon, command=paste)
 edit_menu.add_separator()#add a separator
-edit_menu.add_command(label='Find', underline=0, accelerator='Ctrl+F')
+edit_menu.add_command(label='Find', underline=0, accelerator='Ctrl+F', command=find_text)
 edit_menu.add_separator()#add a separator
 edit_menu.add_command(label='Select All', underline=7, accelerator='Ctrl+A', command=select_all)
 menu_bar.add_cascade(label='Edit', menu=edit_menu)
@@ -92,7 +134,6 @@ show_line_no = IntVar()
 show_line_no.set(1)
 #adding a checkbutton that allows the user to toggle the show line number function 
 view_menu.add_checkbutton(label="Show Line Numbers", variable=show_line_no)
-
 #acreating a variable that stores the state of show_cursor_info
 show_cursor_info = IntVar()
 #set the value of the show_cursor_info to '1'
@@ -128,16 +169,18 @@ theme_choice.set('Default')
 for k in sorted(color_schemes):
 	#adding a radio button that allows user to select the colour scheme
 	themes_menu.add_radiobutton(label=k, variable=theme_choice)
-
 #adding the above widgets to the view menu
 menu_bar.add_cascade(label='View', menu=view_menu)
-
 
 #adding and populating an about menu in the menu bar
 about_menu = Menu(menu_bar, tearoff=0)
 about_menu.add_command(label='About')
 about_menu.add_command(label='Help')
 menu_bar.add_cascade(label='About',  menu=about_menu)
+
+#displaying the menu
+root.config(menu=menu_bar)
+
 
 #adding a frame for the horizontal shortcut icon bar with styling
 shortcut_bar = Frame(root, height=25, background='#f0f0f0')
@@ -154,18 +197,20 @@ content_text = Text(root, wrap='word', undo=1)
 #display the main content Text widget
 content_text.pack(expand='yes', fill='both')
 #adding a scroll bar widget to the main text content widget
-content_text.bind('Control-y', redo)
-content_text.bind('Control-Y', redo)
-content_text.bind('Control-a', select_all)
-content_text.bind('Control-A', select_all)
 scroll_bar = Scrollbar(content_text)
 content_text.configure(yscrollcommand=scroll_bar.set)
 scroll_bar.config(command=content_text.yview)
 #display the scroll bar
 scroll_bar.pack(side='right', fill='y')
 
-#displaying the menu
-root.config(menu=menu_bar)
+#adding key bindings for the operations
+content_text.bind('<Control-y>', redo)
+content_text.bind('<Control-Y>', redo)
+content_text.bind('<Control-a>', select_all)
+content_text.bind('<Control-A>', select_all)
+content_text.bind('<Control-f>', find_text)
+content_text.bind('<Control-F>', find_text)
+
 
 #execute the mainloop() function
 root.mainloop()
